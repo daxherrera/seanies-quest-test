e57ba18f193d14f0c6b7ea0791e722c4df0f3e9c
daxherrera <daxherrera@gmail.com>
2024-01-21 21:28:13 -0800

    a

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { resolve } = require('path');
-//const writetest = require('./server.js');
+const readtest = require('./server.js');
 
 const app = express();
 const port = 3010;
@@ -8,13 +8,11 @@ const port = 3010;
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  (async () => {
     console.log('before start');
 
-    //await writetest();
+    //await readtest();
 
     console.log('after start');
-  })();
 
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
